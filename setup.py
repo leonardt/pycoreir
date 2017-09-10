6f28535af1fede3484c3a8c8348c5a65b01c886b
Lenny Truong <lenny@cs.stanford.edu>
2017-09-10 09:27:44 -0700

    deployment updates

--- setup.py
+++ setup.py
@@ -4,5 +4,9 @@ setup(
     name='coreir',
     version='0.1-alpha',
     description='Python bindings for CoreIR',
-    packages=["coreir"]
+    packages=["coreir"],
+    license='BSD License',
+    url='https://github.com/leonardt/pycoreir',
+    author='Leonard Truong',
+    author_email='lenny@cs.stanford.edu'
 )
