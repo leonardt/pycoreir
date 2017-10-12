31eecc903c441efd4269442a002435a091ad4ffa
Lenny Truong <leonardtruong@protonmail.com>
2017-10-12 16:05:51 -0700

    Bump version again due to tag mismatch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coreir',
-    version='0.06-alpha',
+    version='0.07-alpha',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
