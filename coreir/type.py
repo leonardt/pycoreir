be825d16574fef77f64a89d261735738487e75f7
Lenny Truong <leonardtruong@protonmail.com>
2017-10-12 19:06:07 -0700

    Update value ctype for bitvector

--- coreir/type.py
+++ coreir/type.py
@@ -29,7 +29,7 @@ class Value(CoreIRType):
             return libcoreir_c.COREValueIntGet(self.ptr)
         elif type == 2:
             width = ct.c_int()
-            value = ct.c_int()
+            value = ct.c_uint64()
             libcoreir_c.COREValueBitVectorGet(self.ptr, ct.byref(width), ct.byref(value))
             return BitVector(width.value, value.value)
         elif type == 3:
