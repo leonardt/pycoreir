b57ffea242ec397405a699fcf6c174e2a89df640
Lenny Truong <lenny@cs.stanford.edu>
2017-10-27 15:59:14 -0700

    Add typegen note

--- coreir/type_gen.py
+++ coreir/type_gen.py
@@ -5,6 +5,10 @@ import ctypes
 def type_gen(fn):
     @wraps(fn)
     def wrapped(context, names, values, num_values):
+        """
+        Only use these arguments in this function, coreir will cleanup the
+        pointers after this function returns, DO NOT store
+        """
         context = coreir.Context(ctypes.cast(context, coreir.COREContext_p))
         values = ctypes.cast(values, ctypes.POINTER(coreir.COREValue_p))
         names = ctypes.cast(names, ctypes.POINTER(ctypes.c_char_p))
