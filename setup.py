c227b77276ea7184d8d4dcd4af2798a30d5389ce
Lenny Truong <lenny@cs.stanford.edu>
2018-01-29 16:40:35 -0800

    Bump version for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coreir',
-    version='0.13-alpha',
+    version='0.14-alpha',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
