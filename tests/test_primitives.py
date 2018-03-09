722cfd64c187ad64821d383ea89af0a31a334995
Lenny Truong <lenny@stanford.edu>
2018-03-08 17:39:58 -0800

    Add comparison ops

--- tests/test_primitives.py
+++ tests/test_primitives.py
@@ -2,6 +2,19 @@ import coreir
 from bit_vector import BitVector
 import operator
 
+def get_lib(lib, context):
+    if lib in {"coreir", "mantle", "corebit"}:
+        return context.get_namespace(lib)
+    elif lib == "global":
+        return context.global_namespace
+    else:
+        return context.load_library(lib)
+
+def import_(lib, name):
+    context = coreir.Context()
+    lib = get_lib(lib, context)
+    return lib.generators[name], context
+
 binary_primitives = [
     ("and", operator.and_),
     ("or", operator.or_),
@@ -13,33 +26,34 @@ binary_primitives = [
     ("lshr", operator.rshift),
 ]
 
+comparison_primitives = [
+    ("slt", operator.lt, True),
+    ("sle", operator.le, True),
+    ("sgt", operator.gt, True),
+    ("sge", operator.ge, True),
+    ("ult", operator.lt, False),
+    ("ule", operator.le, False),
+    ("ugt", operator.gt, False),
+    ("uge", operator.ge, False),
+]
+
 def pytest_generate_tests(metafunc):
     if 'binary_primitive' in metafunc.fixturenames:
         metafunc.parametrize("binary_primitive", binary_primitives)
+    if 'comparison_primitive' in metafunc.fixturenames:
+        metafunc.parametrize("comparison_primitive", comparison_primitives)
     if 'width' in metafunc.fixturenames:
         metafunc.parametrize("width", [4])
-    if 'binary_input0' in metafunc.fixturenames:
-        metafunc.parametrize("binary_input0", range(16))
-    if 'binary_input1' in metafunc.fixturenames:
-        metafunc.parametrize("binary_input1", range(16))
+    if 'input0' in metafunc.fixturenames:
+        metafunc.parametrize("input0", range(16))
+    if 'input1' in metafunc.fixturenames:
+        metafunc.parametrize("input1", range(16))
 
 
-def test_binary_primitive(binary_primitive, width, binary_input0, binary_input1):
+def test_binary_primitive(binary_primitive, width, input0, input1):
 
-    context = coreir.Context()
-
-    def get_lib(lib):
-        if lib in {"coreir", "mantle", "corebit"}:
-            return context.get_namespace(lib)
-        elif lib == "global":
-            return context.global_namespace
-        else:
-            return context.load_library(lib)
-
-    def import_(lib, name):
-        return get_lib(lib).generators[name]
     primitive_name, primitive_op = binary_primitive
-    coreir_primitive = import_("coreir", primitive_name)
+    coreir_primitive, context = import_("coreir", primitive_name)
     primitive16 = coreir_primitive(width=width)
     module_typ = context.Record({
         "in0": context.Array(width, context.BitIn()),
@@ -56,8 +70,63 @@ def test_binary_primitive(binary_primitive, width, binary_input0, binary_input1)
     module.definition = module_def
     sim_primitive16 = coreir.SimulatorState(module)
 
-    a = BitVector(binary_input0, 16)
-    b = BitVector(binary_input1, 16)
+    a = BitVector(input0, 16)
+    b = BitVector(input1, 16)
+    sim_primitive16.set_value(["self.in0"], a.as_bool_list())
+    sim_primitive16.set_value(["self.in1"], b.as_bool_list())
+    sim_primitive16.execute()
+    assert BitVector(sim_primitive16.get_value(["self"], ["out"])) == primitive_op(a, b)
+
+def test_eq(width, input0, input1):
+
+    coreir_primitive, context = import_("coreir", "eq")
+    primitive_name = "eq"
+    primitive_op = operator.eq
+    primitive16 = coreir_primitive(width=width)
+    module_typ = context.Record({
+        "in0": context.Array(width, context.BitIn()),
+        "in1": context.Array(width, context.BitIn()),
+        "out": context.Bit()
+    })
+    module = context.global_namespace.new_module(f"test_{primitive_name}", module_typ)
+    module_def = module.new_definition()
+    primitive16_inst = module_def.add_module_instance(f"{primitive_name}_inst", primitive16)
+    interface = module_def.interface
+    module_def.connect(interface.select("in0"), primitive16_inst.select("in0"))
+    module_def.connect(interface.select("in1"), primitive16_inst.select("in1"))
+    module_def.connect(interface.select("out"), primitive16_inst.select("out"))
+    module.definition = module_def
+    sim_primitive16 = coreir.SimulatorState(module)
+
+    a = BitVector(input0, 16)
+    b = BitVector(input1, 16)
+    sim_primitive16.set_value(["self.in0"], a.as_bool_list())
+    sim_primitive16.set_value(["self.in1"], b.as_bool_list())
+    sim_primitive16.execute()
+    assert BitVector(sim_primitive16.get_value(["self"], ["out"])) == primitive_op(a, b)
+
+def test_comparison_primitive(comparison_primitive, width, input0, input1):
+
+    primitive_name, primitive_op, signed = comparison_primitive
+    coreir_primitive, context = import_("coreir", primitive_name)
+    primitive16 = coreir_primitive(width=width)
+    module_typ = context.Record({
+        "in0": context.Array(width, context.BitIn()),
+        "in1": context.Array(width, context.BitIn()),
+        "out": context.Bit()
+    })
+    module = context.global_namespace.new_module(f"test_{primitive_name}", module_typ)
+    module_def = module.new_definition()
+    primitive16_inst = module_def.add_module_instance(f"{primitive_name}_inst", primitive16)
+    interface = module_def.interface
+    module_def.connect(interface.select("in0"), primitive16_inst.select("in0"))
+    module_def.connect(interface.select("in1"), primitive16_inst.select("in1"))
+    module_def.connect(interface.select("out"), primitive16_inst.select("out"))
+    module.definition = module_def
+    sim_primitive16 = coreir.SimulatorState(module)
+
+    a = BitVector(input0, 16, signed=signed)
+    b = BitVector(input1, 16, signed=signed)
     sim_primitive16.set_value(["self.in0"], a.as_bool_list())
     sim_primitive16.set_value(["self.in1"], b.as_bool_list())
     sim_primitive16.execute()
