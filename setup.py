6cc7fe9a61e55e18d51cf8b258baa2b98b6f22f6
Lenny Truong <lenny@stanford.edu>
2018-04-09 23:14:19 -0700

    Add bit_vector package as Python requirement

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup
+from pip.req import parse_requirements
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+
+install_requires = []
+extra_requires = {}
+for item in parse_requirements("requirements.txt", session=False):
+    req = str(item.req)
+    if item.markers is not None:
+        req += ";" + str(item.markers)
+    install_requires.append(req)
 
 setup(
     name='coreir',
@@ -8,5 +19,6 @@ setup(
     license='BSD License',
     url='https://github.com/leonardt/pycoreir',
     author='Leonard Truong',
-    author_email='lenny@cs.stanford.edu'
+    author_email='lenny@cs.stanford.edu',
+    install_requires=install_requires
 )
