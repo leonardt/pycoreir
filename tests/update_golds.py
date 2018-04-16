428ca376c5795a7f2e5b57c8e366fa6c1b9fbff6
Lenny Truong <lenny@stanford.edu>
2018-04-15 18:45:58 -0700

    Add mapParallel gold to update script

--- tests/update_golds.py
+++ tests/update_golds.py
@@ -1,6 +1,7 @@
 import shutil
 test_pairs = [
-    ("ice40_test.json", "ice40_test_gold.json")
+    ("ice40_test.json", "ice40_test_gold.json"),
+    ("mapParallel_test.json", "mapParallel_test_gold.json")
 ]
 
 for output, gold in test_pairs:
