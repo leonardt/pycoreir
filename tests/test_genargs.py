1f94696604993b4c868e487b2ecafa4a518ac5ce
Lenny Truong <lenny@stanford.edu>
2018-04-18 16:17:34 -0700

    update genargs test

--- tests/test_genargs.py
+++ tests/test_genargs.py
@@ -4,15 +4,18 @@ import os
 
 def test_genargs():
     context = coreir.Context()
-    cgra = context.load_library("cgralib")
+    commonlib = context.load_library("commonlib")
     mod = context.load_from_file(os.path.join(os.path.dirname(os.path.realpath(__file__)), "genargs.json"))
     for instance in mod.definition.instances:
         for name, arg in instance.module.generator_args.items():
-            if name == "width":
-                assert arg.value == 4
-            elif name in {"has_en", "has_clr", "has_rst"}:
-                assert instance.module.name == "reg"
-                assert arg.value == False
+            if name == "bitwidth":
+                assert arg.value == 16
+            elif name == "image_width":
+                assert arg.value == 10
+            elif name == "stencil_height":
+                assert arg.value == 3
+            elif name == "stencil_width":
+                assert arg.value == 1
             else:
                 assert False, "Should not reach this statement, {}".format(name)
 
