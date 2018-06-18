c07c731823e53604d7d44e074960b9d1b8a49dfe
Lenny Truong <lenny@stanford.edu>
2018-06-18 14:44:48 -0700

    Fix install coreir logic for tags

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='coreir',
-    version='0.19-alpha',
+    version='0.20-alpha',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
