14d1bffcaca7d651d2bfaef2c98833dd547d321f
Lenny Truong <lenny@stanford.edu>
2018-08-11 17:51:46 -0700

    Fix bit_vector version in setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(
     url='https://github.com/leonardt/pycoreir',
     author='Leonard Truong',
     author_email='lenny@cs.stanford.edu',
-    install_requires=["bit_vector"]
+    install_requires=["bit_vector==0.30a"]
 )
