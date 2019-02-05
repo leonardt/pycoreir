0e43aec5ce4e07fedfe8fc25b1342bf47fc58fde
Lenny Truong <lenny@stanford.edu>
2019-02-04 18:58:03 -0800

    Add README as long description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='coreir',
     version='1.0.0',
@@ -9,5 +12,7 @@ setup(
     url='https://github.com/leonardt/pycoreir',
     author='Leonard Truong',
     author_email='lenny@cs.stanford.edu',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=["bit_vector >= 0.39a, <=0.42a0"]
 )
