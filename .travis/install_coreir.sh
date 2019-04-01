facf311eaa486b2660d6f832f69b8d64c0c56e9b
Lenny Truong <lenny@stanford.edu>
2019-04-01 11:22:19 -0700

    Use cmake build on travisg

--- .travis/install_coreir.sh
+++ .travis/install_coreir.sh
@@ -6,11 +6,13 @@ if [ "$TRAVIS_BRANCH" == "dev" ]; then
     cd deps;
     git clone -b dev https://github.com/rdaly525/coreir.git;
     cd coreir;
-    export COREIRCONFIG="g++-4.9";
-    export COREIR=$PWD;
+    export CXX="g++-4.9";
+    cd build
+    cmake ..
     make install prefix=$TRAVIS_BUILD_DIR/deps;
-    cd ..;
-    cd ..;
+    cd ..;  # build
+    cd ..;  # coreir
+    cd ..;  # deps
 else
     # based on https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
     curl -s -L https://github.com/rdaly525/coreir/releases/latest | grep "href.*coreir.tar.gz" | cut -d \" -f 2 | xargs -I {} wget https://github.com"{}"
