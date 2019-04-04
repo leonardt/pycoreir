8ab3adf4d20ccab265fb1fd393bd12c7b5b9ee80
Ross <rdaly525@stanford.edu>
2019-04-04 13:35:26 -0700

    testing on latest release

--- .travis/install_coreir.sh
+++ .travis/install_coreir.sh
@@ -11,8 +11,18 @@ if [ "$TRAVIS_BRANCH" == "dev" ]; then
     make install prefix=$TRAVIS_BUILD_DIR/deps;
     cd ..;
     cd ..;
-else
+elif [ "$TRAVIS_BRANCH" == "test_release" ]; then
     # based on https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
+    wget https://github.com/rdaly525/coreir/releases/download/v0.1.0/coreir.tar.gz;
+    mkdir coreir_release;
+    tar -xf coreir.tar.gz -C coreir_release --strip-components 1;
+    cd coreir_release;
+    make DESTDIR=$TRAVIS_BUILD_DIR/deps install
+    cd ..
+    pip install coreir;
+fielse
+    # based on https://gist.github.com/steinwaywhw/a4cd19cda655b8249d908261a62687f8
+else
     curl -s -L https://github.com/rdaly525/coreir/releases/latest | grep "href.*coreir.tar.gz" | cut -d \" -f 2 | xargs -I {} wget https://github.com"{}"
     mkdir coreir_release;
     tar -xf coreir.tar.gz -C coreir_release --strip-components 1;
