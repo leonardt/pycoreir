ea5e1c1c4f359fcb933eadf7441b142bd0d4b5ca
Keyi Zhang <keyi@stanford.edu>
2019-04-04 23:29:27 -0700

    add more libs

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ class CoreIRExtension(Extension):
 
 
 class CoreIRBuild(build_ext):
-    libs = ["coreir-c", "coreirsim-c", "coreir-ice40"]
+    libs = ["coreir-c", "coreirsim-c", "coreir-ice40", "coreir-aetherlinglib"]
     def run(self):
         if not os.path.isdir(COREIR_PATH):
             subprocess.check_call(["git", "clone", "--depth=1", COREIR_REPO,
