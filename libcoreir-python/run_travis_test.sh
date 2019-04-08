1a8bac9b860ff5c52096f61bac80ddf59bcc159a
Lenny Truong <lenny@stanford.edu>
2019-04-08 15:50:13 -0700

    Try using explicit paths for travis test

--- libcoreir-python/run_travis_test.sh
+++ libcoreir-python/run_travis_test.sh
@@ -1,2 +1,2 @@
-make install
-PYTHONPATH=$PYTHONPATH:test make test
+make install prefix=/pycoreir/coreir-cpp/include LDFLAGS=-L/pycoreir/coreir-cpp/lib
+PYTHONPATH=$PYTHONPATH:test make test CXXFLAGS=-I/pycoreir/coreir-cpp/include LDFLAGS=-L/pycoreir/coreir-cpp/lib
