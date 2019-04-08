89e670396f9829417eb9b84f3be3fe29acc0509b
Lenny Truong <lenny@stanford.edu>
2019-04-08 16:33:27 -0700

    Fixup make flags

--- libcoreir-python/run_travis_test.sh
+++ libcoreir-python/run_travis_test.sh
@@ -1,2 +1,2 @@
-make install prefix=/pycoreir/coreir-cpp/include LDFLAGS=-L/pycoreir/coreir-cpp/lib
-PYTHONPATH=$PYTHONPATH:test make test CXXFLAGS=-I/pycoreir/coreir-cpp/include LDFLAGS=-L/pycoreir/coreir-cpp/lib
+make install PYTHON_CONFIG=python3-config prefix=/pycoreir/coreir-cpp/include CXXFLAGS=-I/pycoreir/coreir-cpp/include LDFLAGS=-L/pycoreir/coreir-cpp/lib
+PYTHONPATH=$PYTHONPATH:test make test PYTHON_CONFIG=python3-config CXXFLAGS=-I/pycoreir/coreir-cpp/include LDFLAGS=-L/pycoreir/coreir-cpp/lib
