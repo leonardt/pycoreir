5b977728f134dd625d314f33c7c4ffc5979f408a
Ross <rdaly525@stanford.edu>
2019-04-09 14:58:17 -0700

    added metaclass to memoize instances. added tests to test_base

--- coreir/base.py
+++ coreir/base.py
@@ -2,20 +2,28 @@ import ctypes as ct
 from .lib import libcoreir_c
 import coreir
 
-
 def get_pointer_value(pointer):
     """
     Converts ctypes pointer to its underlying integer representation
     """
     return ct.cast(pointer, ct.c_void_p).value
 
+_cache = {}
+class Memoize(type):
+    def __call__(cls,ptr,*args,**kwargs):
+        cptr = get_pointer_value(ptr)
+        if cptr in _cache:
+            return _cache[cptr]
+        else:
+            inst = super().__call__(ptr,*args,**kwargs)
+            _cache[cptr] = inst
+        return inst
 
-class CoreIRType(object):
+class CoreIRType(metaclass=Memoize):
     def __init__(self, ptr, context):
         self.ptr = ptr
-        assert isinstance(context, coreir.context.Context)
         self.context = context
-
+      
     def __hash__(self):
         return get_pointer_value(self.ptr)
 
