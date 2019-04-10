f3c8d31c7035500b296e6979146f250b1c5aa77b
Lenny Truong <lenny@stanford.edu>
2019-04-10 09:27:17 -0700

    Release hotfix

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coreir',
-    version='2.0.4',
+    version='2.0.5',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
