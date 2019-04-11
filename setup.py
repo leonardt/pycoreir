a08adc277790df6d2e5e5761788897dc837fb43a
Lenny Truong <lenny@stanford.edu>
2019-04-11 13:06:07 -0700

    Bump version to test release

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coreir',
-    version='2.0.5',
+    version='2.0.6',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
