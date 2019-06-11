fc2474b96e95f8f6e75327712744b9d699cfd377
Lenny Truong <lenny@stanford.edu>
2019-06-10 19:08:19 -0700

    Update run.sh

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    docker exec manylinux pip install magma-lang  # For libcoreir-python test
-    docker exec manylinux pytest -s /pycoreir/tests/
+    docker exec garnet-flow pip install magma-lang  # For libcoreir-python test
+    docker exec garnet-flow pytest -s /pycoreir/tests/
 else
     # osx
     pip install magma-lang
