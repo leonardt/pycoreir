393b2cbb8c44eecefa878c05cb82e06635037453
Lenny Truong <leonardtruong@fb.com>
2019-08-06 13:30:51 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -94,7 +94,7 @@ if not shutil.which("coreir"):
 
 setup(
     name='coreir',
-    version='2.0.24',
+    version='2.0.25',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
