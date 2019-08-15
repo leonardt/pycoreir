8e03583769ae3da6994b47e907a565c3afbb9a99
Keyi Zhang <keyi@stanford.edu>
2019-08-14 19:21:20 -0700

    use old deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,8 +6,23 @@ echo "  pypi"                                    >> ~/.pypirc
 echo                                             >> ~/.pypirc
 echo [pypi]                                      >> ~/.pypirc
 echo repository=https://upload.pypi.org/legacy/  >> ~/.pypirc
-echo username=keyi                               >> ~/.pypirc
+echo username=leonardt                           >> ~/.pypirc
 echo password=$PYPI_PASSWORD                     >> ~/.pypirc
 
-python setup.py sdist bdist_wheel
-twine upload dist/*
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+    docker cp ~/.pypirc manylinux:/home/
+    docker exec -i manylinux bash -c 'pip install twine'
+    docker exec -i manylinux bash -c 'cd  /pycoreir && twine upload --config-file /home/.pypirc wheelhouse/*'
+
+    # Upload source distribution too
+    docker exec -i manylinux bash -c 'cd  /pycoreir && python setup.py sdist'
+    docker exec -i manylinux bash -c 'cd  /pycoreir && twine upload --config-file /home/.pypirc dist/*.tar.gz'
+else
+    export PYTHON=3.7.0
+    export PYENV_VERSION=$PYTHON
+    export PATH="/Users/travis/.pyenv/shims:${PATH}"
+    source /Users/travis/.pyenv/versions/${PYTHON}/envs/venv/bin/activate
+    # osx
+    pip install twine
+    twine upload dist/*
+fi
