e9c7049271eb94987bf459671ccf6f55bdfc77e0
Keyi Zhang <keyi@stanford.edu>
2019-08-14 18:45:11 -0700

    change the pypi user name

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,7 +6,7 @@ echo "  pypi"                                    >> ~/.pypirc
 echo                                             >> ~/.pypirc
 echo [pypi]                                      >> ~/.pypirc
 echo repository=https://upload.pypi.org/legacy/  >> ~/.pypirc
-echo username=leonardt                           >> ~/.pypirc
+echo username=keyi                               >> ~/.pypirc
 echo password=$PYPI_PASSWORD                     >> ~/.pypirc
 
 python setup.py sdist bdist_wheel
