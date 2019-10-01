5394833960c9a50b88e7a540305d1fe44886e048
Caleb Donovick <donovick@cs.stanford.edu>
2019-10-01 13:42:38 -0700

    Use pytest.raises

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,3 +1,5 @@
+import pytest
+
 import coreir
 from coreir.module import NotAGeneratorException
 def test_module():
@@ -5,8 +7,5 @@ def test_module():
     module_typ = c.Record({"input": c.Array(8, c.BitIn()), "output": c.Array(9, c.Bit())})
     module = c.global_namespace.new_module("multiply_by_2", module_typ)
     assert module.generated == False
-    try:
-        generator_args = module.generator_args
-        assert False, "Should throw an exception for a non generator module"
-    except NotAGeneratorException:
-        pass
+    with pytest.raises(NotAGeneratorException):
+        module.generator_args
