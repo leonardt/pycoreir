61a33fe2d524f50be71282008ddd2c4ab0f59d3a
Keyi Zhang <keyi@stanford.edu>
2019-10-06 19:14:45 -0700

    uninstall coreir from the docker container

--- .travis/run.sh
+++ .travis/run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     docker exec garnet-flow pip install magma-lang  # For libcoreir-python test
+    docker exec garnet-flow pip uninstall -y coreir    # remove coreir dep since it's not working properly
     docker exec garnet-flow bash -c "cd /pycoreir && pytest -s tests/"
 else
     # osx
