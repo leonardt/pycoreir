47ea76e07ba738eb66ff41129f6dee0d15926271
Keyi Zhang <keyi@stanford.edu>
2019-10-06 20:07:23 -0700

    fix pypi username

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,17 +6,13 @@ echo "  pypi"                                    >> ~/.pypirc
 echo                                             >> ~/.pypirc
 echo [pypi]                                      >> ~/.pypirc
 echo repository=https://upload.pypi.org/legacy/  >> ~/.pypirc
-echo username=leonardt                           >> ~/.pypirc
+echo username=keyi                               >> ~/.pypirc
 echo password=$PYPI_PASSWORD                     >> ~/.pypirc
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     docker cp ~/.pypirc manylinux:/home/
     docker exec -i manylinux bash -c 'pip install twine'
     docker exec -i manylinux bash -c 'cd  /pycoreir && twine upload --config-file /home/.pypirc wheelhouse/*'
-
-    # Upload source distribution too
-    docker exec -i manylinux bash -c 'cd  /pycoreir && python setup.py sdist'
-    docker exec -i manylinux bash -c 'cd  /pycoreir && twine upload --config-file /home/.pypirc dist/*.tar.gz'
 else
     export PYTHON=3.7.0
     export PYENV_VERSION=$PYTHON
