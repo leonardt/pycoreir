5fd92b9e9c1932ab5fcb64306f70727f89cc542d
Keyi Zhang <keyi@stanford.edu>
2020-01-06 11:05:11 -0800

    fix python wheel

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    docker pull keyiz/manylinux-coreir
+    docker pull keyiz/manylinux
     docker pull keyiz/garnet-flow
-    docker run -d --name manylinux --rm -it --mount type=bind,source="$(pwd)"/../pycoreir,target=/pycoreir keyiz/manylinux-coreir bash
+    docker run -d --name manylinux --rm -it --mount type=bind,source="$(pwd)"/../pycoreir,target=/pycoreir keyiz/manylinux bash
     docker run -d --name garnet-flow --rm -it --mount type=bind,source="$(pwd)"/../pycoreir,target=/pycoreir keyiz/garnet-flow bash
     docker cp ../pycoreir manylinux:/
     docker exec manylinux bash -c "cd pycoreir && python setup.py bdist_wheel"
     docker exec manylinux bash -c "pip install auditwheel"
     docker exec manylinux bash -c "auditwheel show /pycoreir/dist/*.whl"
     # we should have any external linked libraries at this point
-    docker exec manylinux bash -c "cd pycoreir && auditwheel repair dist/*.whl"
+    docker exec manylinux bash -c "cd pycoreir && LD_LIBRARY_PATH=/pycoreir/coreir-cpp/build/lib  auditwheel repair dist/*.whl"
     # install the wheel for testing
     # use garnetflow container to test since it has all the prereqs
     docker exec garnet-flow bash -c "cd pycoreir && pip install wheelhouse/*.whl"
