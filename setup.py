1431be7347d5ee620c23da6eb4a35a624fee703b
Lenny Truong <leonardtruong@fb.com>
2020-01-06 15:46:19 -0800

    Bump version for release

--- setup.py
+++ setup.py
@@ -93,7 +93,7 @@ if False:
 
 setup(
     name='coreir',
-    version='2.0.29',
+    version='2.0.30',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
