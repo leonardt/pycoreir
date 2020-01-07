6fcb4e60fb4aac1100fce877b8dff64f1d71c28e
Lenny Truong <lenny@stanford.edu>
2020-01-07 15:37:57 -0800

    Remove old logic

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coreir',
-    version='2.0.44',
+    version='2.0.45',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
