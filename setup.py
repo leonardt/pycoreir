1088ca1f03e64dd6fcb24583c27b11b8610da7d6
Lenny Truong <leonardtruong@fb.com>
2020-02-21 11:14:32 -0800

    Release 2.0.58

--- setup.py
+++ setup.py
@@ -112,7 +112,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coreir',
-    version='2.0.57',
+    version='2.0.58',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
