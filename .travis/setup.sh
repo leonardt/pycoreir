a28bbc468e680744fea5a8e0e2d305c86b3aef0c
Rajsekhar Setaluri <rajsekhar@fb.com>
2020-04-22 23:03:13 -0700

    latest

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -13,6 +13,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     docker exec manylinux bash -c "cd pycoreir && LD_LIBRARY_PATH=/pycoreir/coreir-cpp/build/lib:/opt/rh/devtoolset-2/root/usr/lib64:/opt/rh/devtoolset-2/root/usr/lib:/usr/local/lib64:/usr/local/lib auditwheel repair dist/*.whl"
     # install the wheel for testing
     # use garnetflow container to test since it has all the prereqs
+    docker exec manylinux bash -c "cd pycoreir && git add wheelhouse && git commit -m 'add dist [ci skip]' && git push"
     docker exec garnet-flow bash -c "cd pycoreir && pip install wheelhouse/*.whl"
     docker exec garnet-flow pip install pytest
 else
