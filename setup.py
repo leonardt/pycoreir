bd17c416136584809c49b7709cbe5307f374df4d
Ross <rdaly525@stanford.edu>
2020-07-08 15:40:31 -0700

    Bumped release

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coreir',
-    version='2.0.85',
+    version='2.0.86',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
