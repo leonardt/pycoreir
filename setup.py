2f42b44a9fdc9840a7f46991cd506c130b2a86c5
Lenny Truong <lenny@stanford.edu>
2020-08-06 17:32:08 -0700

    Release 2.0.98

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coreir',
-    version='2.0.97',
+    version='2.0.98',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
