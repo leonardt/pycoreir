d90047ed82ff9b486216e57ca4d21c03f72b2fc8
Ross <rdaly525@stanford.edu>
2020-10-27 11:09:56 -0700

    bumps version

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coreir',
-    version='2.0.125',
+    version='2.0.126',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
