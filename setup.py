42e0a5d1034708deccc85e789a787270cc580827
Raj Setaluri <raj.setaluri@gmail.com>
2022-06-27 15:53:17 -0700

    Relase 2.0.149

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='coreir',
-    version='2.0.148',
+    version='2.0.149',
     description='Python bindings for CoreIR',
     packages=["coreir"],
     license='BSD License',
